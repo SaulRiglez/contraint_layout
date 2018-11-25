7413ea6e16dd68b68908ba5a637dc5ab19a09e8b
SaulRiglez <saul.riglez@gmail.com>
2018-11-25 16:56:09 -0500

    Click listener button.

---
+++ app/src/main/java/com/yoprogramo/diceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.yoprogramo.diceroller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked1", Toast.LENGTH_LONG).show()
+        }
+    }
+}
