148a38f4227357ba390a60f05e9deb4b06327e05
SaulRiglez <saul.riglez@gmail.com>
2018-11-26 21:00:03 -0500

    initlate variable use.

--- app/src/main/java/com/yoprogramo/diceroller/MainActivity.kt
+++ app/src/main/java/com/yoprogramo/diceroller/MainActivity.kt
@@ -3,19 +3,36 @@ package com.yoprogramo.diceroller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rollButton: Button
+    lateinit var resultImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val resultText: TextView = findViewById(R.id.result_text)
+       initViews();
 
         rollButton.setOnClickListener {
-            resultText.text = rollDice().toString()
+            resultImage.setImageResource(setDrawable(rollDice()))
         }
+    }
+
+    private fun initViews() {
+        rollButton = findViewById(R.id.roll_button)
+        resultImage = findViewById(R.id.dice_image)
+    }
+
+    private fun setDrawable(rollDice: Int) = when (rollDice) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> R.drawable.empty_dice
     }
 
     private fun rollDice() = Random().nextInt(6) + 1
